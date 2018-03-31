b65324f24e8de02b553279d557f21c3dc981a020
manualof@qq.com <manualof@qq.com>
2018-03-31 09:30:47 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,20 @@
+<nav  class="navbar navbar-expand-sm bg-light navbar-light navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                LaraBBS
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse justify-content-end" id="app-navbar-collapse">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav justify-content-end">
+                <!-- Authentication Links -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+            
+        </div>
+    </div>
+</nav>
